69acad57eda1ab8434299dfbe9c1a033b18e620d
Crawcik <crawcik@gmail.com>
2020-09-26 23:26:00 +0200

    Added pending messages

--- AnthillNet/Core/Connection.cs
+++ AnthillNet/Core/Connection.cs
@@ -8,17 +8,26 @@ namespace AnthillNet.Core
         public Connection(EndPoint EndPoint)
         {
             this.EndPoint = EndPoint as IPEndPoint;
-            this.messages = new List<Message>();
+            this.messages_received = new List<Message>();
+            this.messages_tosend = new List<Message>();
         }
         public IPEndPoint EndPoint { private set; get; }
-        private readonly List<Message> messages;
+        private readonly List<Message> messages_received;
+        private readonly List<Message> messages_tosend;
+        public int MessagesCount => messages_received.Count;
 
-        public void Add(Message message) => messages.Add(message);
-        public int MessagesCount => messages.Count;
-        public Message[] GetMessages()
+        public void AddReceived(Message message) => messages_received.Add(message);
+        public void AddToSend(Message message) => messages_tosend.Add(message);
+        public Message[] GetMessagesReceived()
         {
-            Message[] results = messages.ToArray();
-            messages.Clear();
+            Message[] results = messages_received.ToArray();
+            messages_received.Clear();
+            return results;
+        }
+        public Message[] GetMessagesToSend()
+        {
+            Message[] results = messages_received.ToArray();
+            messages_received.Clear();
             return results;
         }
 
