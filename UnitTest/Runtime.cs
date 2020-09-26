7ee9a714e82e8a532dc53d57538024d4dd48714e
Crawcik <crawcik@gmail.com>
2020-09-26 17:32:12 +0200

    Writed test funtions for C# 9.0

--- UnitTest/Runtime.cs
+++ UnitTest/Runtime.cs
@@ -1,65 +1,71 @@
 ﻿using System;
-using System.Threading;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
 using System.Linq;
 using AnthillNet.Core;
 
-namespace UnitTest
+Test1();
+
+void Test1()
 {
-    [TestClass]
-    public class Runtime
+    Console.WriteLine("Port:");
+    ushort Port = ushort.Parse(Console.ReadLine());
+    Console.WriteLine("TickRate:");
+    byte TickRate = byte.Parse(Console.ReadLine());
+    Console.WriteLine("Protocol:");
+    ProtocolType Protocol = (ProtocolType)Enum.Parse(typeof(ProtocolType), Console.ReadLine());
+    Server server = new Server(Protocol);
+    server.Logging.LogPriority = LogType.Debug;
+    server.Logging.OnNetworkLog += OnNetworkLog;
+    server.OnRevieceMessage += OnRevieceMessage;
+    server.Init(TickRate);
+    server.Start(Port);
+    Console.WriteLine("Server IP:");
+    string IP= Console.ReadLine();
+    Client client = new Client(ProtocolType.TCP);
+    client.Logging.LogPriority = LogType.Debug;
+    client.Logging.OnNetworkLog += OnNetworkLog;
+    client.OnRevieceMessage += OnRevieceMessage;
+    client.Init(4);
+    client.Connect(IP, 7783);
+loop:
+    if (Console.ReadKey().Key == ConsoleKey.S)
     {
-        public static int message_count;
-
-        [TestMethod]
-        public static void Main()
-        {
-            Test();
-            Thread.Sleep(-1);
-        }
-
-        public static void Test()
-        {
-            Server server = new Server(ProtocolType.TCP);
-            server.Logging.LogPriority = LogType.Debug;
-            server.Logging.OnNetworkLog += OnNetworkLog;
-            server.Init();
-            server.Start(7783);
-            Client client = new Client(ProtocolType.TCP);
-            client.Logging.LogPriority = LogType.Debug;
-            client.Logging.OnNetworkLog += OnNetworkLog;
-            client.Init();
-            client.Connect("127.0.0.1", 7783);
-            client.Send(0, Console.ReadLine());
-        }
-
-        private static void OnRevieceMessage(object sender, Message[] messages)
-        {
-            string message = string.Join("\n", messages.Select(x => (string)x.data));
-            ((Host)sender).Logging.Log("Messages:\n" + message);
-        }
-
-        private static void OnNetworkLog(object sender, NetworkLogArgs e)
-        {
-            Console.Write($"[{e.Time}]");
-            Console.ForegroundColor = ConsoleColor.DarkBlue;
-            Console.Write($"[{e.LogName}]");
-            switch (e.Priority)
-            {
-                case LogType.Info:
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    break;
-                case LogType.Error:
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
-                    break;
-                case LogType.Debug:
-                    Console.ForegroundColor = ConsoleColor.DarkYellow;
-                    break;
-            }
-            Console.Write($"[{e.Priority}]");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write($"{e.Message}\n");
-        }
+        Console.WriteLine("> ");
+        server.Send(0, Console.ReadLine());
     }
+    else if (Console.ReadKey().Key == ConsoleKey.C)
+    {
+        Console.WriteLine("> ");
+        client.Send(0, Console.ReadLine());
+    }
+    else if (Console.ReadKey().Key == ConsoleKey.Q)
+        return;
+goto loop;
+}
+
+void OnRevieceMessage(object sender, Message[] messages)
+{
+    string message = string.Join("\n", messages.Select(x => (string)x.data));
+    ((Host)sender).Logging.Log("Messages:\n" + message);
+}
+
+void OnNetworkLog(object sender, NetworkLogArgs e)
+{
+    Console.Write($"[{e.Time}]");
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.Write($"[{e.LogName}]");
+    switch (e.Priority)
+    {
+        case LogType.Info:
+            Console.ForegroundColor = ConsoleColor.Green;
+            break;
+        case LogType.Error:
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            break;
+        case LogType.Debug:
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            break;
+    }
+    Console.Write($"[{e.Priority}]");
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.Write($"{e.Message}\n");
 }
