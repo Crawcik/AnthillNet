682f204342ae832813d8ab0de9f63f75adcdd343
Crawcik <crawcik@gmail.com>
2020-10-07 11:57:55 +0200

    Adding internal Socket property

--- AnthillNet/Core/Connection.cs
+++ AnthillNet/Core/Connection.cs
@@ -1,25 +1,31 @@
 ﻿using System.Collections.Generic;
 using System.Net;
+using System.Net.Sockets;
 
 namespace AnthillNet.Core
 {
     public struct Connection
     {
-        public Connection(EndPoint EndPoint)
+        public Connection(IPEndPoint address)
         {
-            this.EndPoint = EndPoint as IPEndPoint;
+            this.EndPoint = address;
+            this.messages = null;
+            this.Socket = null;
+        }
+        internal Connection(Socket socket)
+        {
+            this.EndPoint = socket.RemoteEndPoint as IPEndPoint;
             this.messages = new List<Message>();
+            this.Socket = socket;
         }
         public IPEndPoint EndPoint { private set; get; }
+        internal Socket Socket { private set; get; }
         private readonly List<Message> messages;
+
         public int MessagesCount => messages.Count;
+
         internal void Add(Message message) => messages.Add(message);
         internal void ClearMessages() => messages.Clear();
-        internal void ClearData()
-        {
-            this.EndPoint = null;
-            messages.Clear();
-        }
         public Message[] GetMessages()
         {
             Message[] results = messages.ToArray();
