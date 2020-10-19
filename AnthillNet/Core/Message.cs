6b87f59f849f2f91dc5151a281d85a32fa58af0f
Crawcik <crawcik@gmail.com>
2020-10-19 14:55:40 +0200

    Added custom serialization

--- AnthillNet/Core/Message.cs
+++ AnthillNet/Core/Message.cs
@@ -6,10 +6,8 @@ using System.Runtime.Serialization.Formatters.Binary;
 
 namespace AnthillNet.Core
 {
-    [Serializable]
     public struct Message
     {
-        private static readonly BinaryFormatter formatter = new BinaryFormatter();
         public ulong destiny { private set; get; }
         public object data { private set; get; }
 
@@ -19,37 +17,7 @@ namespace AnthillNet.Core
             this.data = data;
         }
 
-        public static Message Deserialize(byte[] raw_data)
-        {
-            Message message;
-            try
-            {
-                message = (Message)formatter.Deserialize(new MemoryStream(raw_data));
-            }
-            catch (SerializationException e)
-            {
-                Console.WriteLine("Failed to serialize. Reason: " + e.Message);
-                throw;
-            }
-            return message;
-        }
-
-        public byte[] Serialize()
-        {
-            MemoryStream stream = new MemoryStream();
-            try
-            {
-                formatter.Serialize(stream, this);
-            }
-            catch (SerializationException e)
-            {
-                throw e;
-            }
-            finally
-            {
-                stream.Close();
-            }
-            return stream.ToArray();
-        }
+        public byte[] Serialize() => Serialization.Serializer.Invoke(this);
+        public static Message Deserialize(byte[] raw_data) => Serialization.Deserializer.Invoke(raw_data);
     }
 }
