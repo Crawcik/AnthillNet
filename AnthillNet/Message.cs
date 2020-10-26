ae54f444867818ca6f85d795cd539116226c9916
Crawcik <crawcik@gmail.com>
2020-10-26 12:23:10 +0100

    Moved from Core namespace

---
+++ AnthillNet/Message.cs
@@ -0,0 +1,18 @@
+﻿namespace AnthillNet
+{
+    [System.Serializable]
+    public struct Message
+    {
+        public readonly string destiny;
+        public readonly object data;
+
+        public Message(string destiny, object data)
+        {
+            this.destiny = destiny;
+            this.data = data;
+        }
+
+        public byte[] Serialize() => Serialization.Serializer.Invoke(this);
+        public static Message Deserialize(byte[] raw_data) => Serialization.Deserializer.Invoke(raw_data);
+    }
+}
