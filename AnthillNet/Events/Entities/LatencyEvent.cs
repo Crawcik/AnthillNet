e510d59c4626a1224a818a0ca6d2ae1ad58f67d7
Crawcik <crawcik@gmail.com>
2020-11-02 13:52:20 +0100

    Fuck it

--- AnthillNet/Events/Entities/LatencyEvent.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AnthillNet.Events.Entities
-{
-    [System.Serializable]
-    public class PingResult_NetArgs : NetArgs
-    {
-        public override void Invoke(INetEvent ev) => ((IPingResult_NetEvent)ev).OnPingResult(this);
-    }
-}
