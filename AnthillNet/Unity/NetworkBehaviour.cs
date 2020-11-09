343941123e429737319db979fab920fb15e97f83
Crawcik <crawcik@gmail.com>
2020-11-09 08:23:48 +0100

    Added behaviour with properties

---
+++ AnthillNet/Unity/NetworkBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using AnthillNet.Events;
+
+using UnityEngine;
+
+namespace AnthillNet.Unity
+{
+    public class NetworkBehaviour : MonoBehaviour
+    {
+        public Order Order => NetworkManager.Instance.Order;
+        public EventManager Event => NetworkManager.Instance.EventManager;
+        public bool IsServer => NetworkManager.Instance.HostType != HostType.Server;
+    }
+}
