78c84ec4d966b92e3dd530ef914a30e124efaafa
Crawcik <crawcik@gmail.com>
2020-11-11 22:37:12 +0100

    Added optional OnTick event

--- AnthillNet/Unity/NetworkBehaviour.cs
+++ AnthillNet/Unity/NetworkBehaviour.cs
@@ -8,6 +8,11 @@ namespace AnthillNet.Unity
     {
         public Order Order => NetworkManager.Instance.Order;
         public EventManager Event => NetworkManager.Instance.EventManager;
-        public bool IsServer => NetworkManager.Instance.HostType != HostType.Server;
+        public bool IsServer => NetworkManager.Instance.HostType == HostType.Server;
+
+        public void SetupTick() => NetworkManager.Instance.Transport.OnTick += Transport_OnTick;
+        protected virtual void OnTick() { }
+
+        private void Transport_OnTick(object sender) => OnTick();
     }
 }
