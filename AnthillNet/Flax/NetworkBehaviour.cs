9c741f1681ec59c160193211a614f089697d77d1
Crawcik <crawcik@gmail.com>
2020-12-20 00:06:36 +0100

    Added basic flax functions

---
+++ AnthillNet/Flax/NetworkBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using AnthillNet.Events;
+
+using FlaxEngine;
+
+namespace AnthillNet.Flax
+{
+    public class NetworkBehaviour : Script
+    {
+        public static Order Order => NetworkManager.Instance.Order;
+        public static bool IsServer => NetworkManager.Instance.HostType == HostType.Server;
+        public void SetupTick() => NetworkManager.Instance.Transport.OnTick += (sender) => OnTick();
+        protected virtual void OnTick() { }
+    }
+}
