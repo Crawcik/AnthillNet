0398043449b6e7cc74f6d36c31f0591be024aa5a
Crawcik <crawcik@gmail.com>
2020-12-19 22:11:42 +0100

    Deleting events

--- AnthillNet/Events/Interfaces.cs
+++
@@ -1,9 +0,0 @@
-﻿using AnthillNet.Events.Entities;
-
-namespace AnthillNet.Events
-{
-    public interface ILatency_NetEvent : INetEvent
-    {
-        void OnLatencyResult(Latency_NetArgs args);
-    }
-}
