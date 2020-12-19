4623eff1deedba2d0ea1bad41242063d48fccca4
Crawcik <crawcik@gmail.com>
2020-12-20 00:08:21 +0100

    Deleting missing function

--- AnthillNet/Unity/NetworkBehaviour.cs
+++ AnthillNet/Unity/NetworkBehaviour.cs
@@ -7,7 +7,6 @@ namespace AnthillNet.Unity
     public class NetworkBehaviour : MonoBehaviour
     {
         public static Order Order => NetworkManager.Instance.Order;
-        public static EventManager Event => NetworkManager.Instance.EventManager;
         public static bool IsServer => NetworkManager.Instance.HostType == HostType.Server;
         public void SetupTick() => NetworkManager.Instance.Transport.OnTick += (sender) => OnTick();
         protected virtual void OnTick() { }
